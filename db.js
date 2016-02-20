28c0815ea8626148dc7a36130dc4b0281573c9d6
Doug Safford <doug.safford@yahoo.com>
2016-02-20 06:36:03 -0600

    Initial commit

---
+++ db.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var url = '';
+//mongoose.connect('mongodb://doug:doug@ds049651.mongolab.com:49651/flights');
+
+
+if(null == process.env.VCAP_SERVICES) {
+    url = 'mongodb://localhost:27017/CardRandomizer';
+}
+else {
+    var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+    url = vcap_services['mongolab'][0].credentials.uri;
+}
+
+//var url = 'mongodb://localhost:27017/DougStudy';
+
+//mongodb://<dbuser>:<dbpassword>@ds037175.mongolab.com:37175/CloudFoundry_l1hnqf0n_mmhir01l
+
+//uri: "mongodb://cloudfoundry-test_2p6otl8c_841b7q4b_tmtlqeaa:eb5d00ac-2a4f-4beb-80ad-9da11cff5a70@ds027908.mongolab.com:27908/cloudfoundry-test_2p6otl8c_841b7q4b"
+
+
+//var url = 'mongodb://doug:doug@ds037175.mongolab.com:37175/CloudFoundry_l1hnqf0n_mmhir01l';
+mongoose.connect(url);
+console.log("In db.js, mongoose connecting");
+
+module.exports = mongoose.connection;
---
+++ routes/index.js
@@ -0,0 +1,321 @@
+var express = require('express');
+//var passport = require('passport');
+//var Account = require('../schemas/account');
+//var entrySchema = require('../schemas/entry');
+//var logUserSchema = require("../schemas/logUser");
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Eat Shot' });
+});
+
+module.exports = router;
+
+//var isAuthenticated = function(req, res, next) {
+//  // if user is authenticated in the session, call the next() to call the next request handler
+//  // Passport adds this method to request object. A middleware is allowed to add properties to
+//  // request and response objects
+//  if (req.isAuthenticated())
+//    return next();
+//  // if the user is not authenticated then redirect the user to the login page
+//  res.redirect('/');
+//}
+
+//function logUser(username, type) {
+//  var record = new logUserSchema();
+//
+//  record.timestamp = Date.now();
+//  record.user = username;
+//  record.type = type;
+//
+//  record.save(function(err) {
+//    if (err) {
+//      console.log(err);
+//      res.status(500).json({
+//        status: 'failure'
+//      });
+//    }
+//  });
+//}
+
+
+//
+//module.exports = function(passport) {
+//module.exports = function() {
+//
+//
+
+  //
+  ///* GET login page. */
+  //router.get('/', function(req, res) {
+  //  // Display the Login page with any flash message, if any
+  //  res.render('index', {
+  //    message: req.flash('message')
+  //  });
+  //});
+  //
+  ///* Handle Login POST */
+  //router.post('/login', passport.authenticate('login', {
+  //  successRedirect: '/homeold',
+  //  failureRedirect: '/',
+  //  failureFlash: true
+  //}));
+  //
+  ///* GET Registration Page */
+  //router.get('/register', function(req, res) {
+  //  res.render('register', {
+  //    message: req.flash('message')
+  //  });
+  //});
+  //
+  ///* Handle Registration POST */
+  //router.post('/register', passport.authenticate('register', {
+  //  successRedirect: '/home',
+  //  failureRedirect: '/register',
+  //  failureFlash: true
+  //}));
+
+  /* GET Home Page */
+  router.get('/home', function(req, res) {
+    res.render('home', {
+      user: req.user
+    });
+  });
+
+  ///* GET Home Page */
+  //router.get('/homeold', isAuthenticated, function(req, res) {
+  //  res.render('homeold', {
+  //    user: req.user
+  //  });
+  //});
+
+  ///* Handle Logout */
+  //router.get('/logout', isAuthenticated, function(req, res) {
+  //  console.log("in logout, user=" + req.user.username);
+  //
+  //  // log the logout
+  //  logUser(req.user.username, "out")
+  //  req.logout();
+  //
+  //
+  //
+  //  res.redirect('home');
+  //});
+
+  /* GET Entry List Page
+   router.get('/entrylist', isAuthenticated, function(req, res) {
+   console.log("in entry list doug");
+
+   entrySchema.find()
+   .setOptions({
+   sort: 'timestamp'
+   })
+   .exec(function(err, entrys) {
+   if (err) {
+   res.status(500).json({
+   status: 'failure'
+   });
+   } else {
+   res.render('entrys', {
+   title: 'entrys',
+   entrys: entrys,
+   user: req.user
+   });
+   }
+   });
+   });
+   */
+  /* GET Entry List Page
+   router.get('/entrylistsorted', isAuthenticated, function(req, res) {
+   console.log("in entry list Sorted doug");
+
+   entrySchema.find({
+   user: 'Dragon'
+   })
+   .setOptions({
+   sort: 'timestamp'
+   })
+   .exec(function(err, entrys) {
+   if (err) {
+   res.status(500).json({
+   status: 'failure'
+   });
+   } else {
+   res.render('entryListSorted', {
+   title: 'Entries Sorted',
+   entrys: entrys,
+   user: req.user
+   });
+   }
+   });
+   });
+   */
+  //// Go to entry input form
+  //router.get('/entryinput', isAuthenticated, function(req, res) {
+  //  console.log("in entry input doug");
+  //  res.render('entryinput', {
+  //    title: 'All Doug entrys',
+  //    user: req.user
+  //  });
+  //});
+  //
+  //// Go to entry edit form
+  //router.get('/entryedit/:id', isAuthenticated, function(req, res) {
+  //  console.log("in function entry edit");
+  //  entrySchema.find({
+  //        _id: req.params.id
+  //      })
+  //      .exec(function(err, entrys) {
+  //        if (err) {
+  //          res.status(500).json({
+  //            status: 'failure'
+  //          });
+  //        } else {
+  //          console.log("not failure getting entry edit");
+  //          res.render('entryedit', {
+  //            title: 'Edit entry',
+  //            entrys: entrys,
+  //            user: req.user
+  //          });
+  //        }
+  //      });
+  //});
+  ///////////////////////////////////////////////////////
+  //// Sorted table edited entry
+  //router.get('/entrylistSorted/:user', isAuthenticated, function(req, res) {
+  //  console.log("In entrylistSorted");
+  //  var myUser = req.params.user;
+  //  console.log("myUser:" + myUser);
+  //  // Return nothing if first time
+  //  if (myUser == '') {
+  //    console.log("In myUser = nil");
+  //    entrySchema.find()
+  //        .setOptions({
+  //          sort: 'timestamp'
+  //        })
+  //        .exec(function(err, entrys) {
+  //          if (err) {
+  //            res.status(500).json({
+  //              status: 'failure'
+  //            });
+  //          } else {
+  //            res.render('entrys', {
+  //              title: 'Entries Sorted',
+  //              entrys: entrys,
+  //              user: req.params.user
+  //            });
+  //          }
+  //        })
+  //    // For now allow returning all
+  //  } else if (myUser == "All") {
+  //    console.log("In myUser = All: " + myUser);
+  //    entrySchema.find()
+  //        .setOptions({
+  //          sort: 'timestamp'
+  //        })
+  //        .exec(function(err, entrys) {
+  //          if (err) {
+  //            res.status(500).json({
+  //              status: 'failure'
+  //            });
+  //          } else {
+  //            res.render('entrys', {
+  //              title: 'entrys',
+  //              entrys: entrys,
+  //              user: "All"
+  //            });
+  //          }
+  //        })
+  //  } else {
+  //    console.log("In myUser = a value=" + myUser);
+  //    entrySchema.find({
+  //          user: myUser
+  //        })
+  //        .setOptions({
+  //          sort: 'timestamp'
+  //        })
+  //        .exec(function(err, entrys) {
+  //          if (err) {
+  //            res.status(500).json({
+  //              status: 'failure'
+  //            });
+  //          } else {
+  //            res.render('entrys',  {
+  //              title: 'entrys',
+  //              entrys: entrys,
+  //              user: myUser
+  //            });
+  //          }
+  //        });
+  //  }
+  //});
+
+
+
+  //// Update edited entry
+  //router.post('/entryedit/:id', isAuthenticated, function(req, res) {
+  //  //console.log("In updateentry");
+  //  entrySchema.findOneAndUpdate({
+  //    _id: req.params.id
+  //  }, {
+  //    timestamp: req.body.timestamp,
+  //    user: req.body.user,
+  //    minutes: req.body.minutes,
+  //    comments: req.body.comments
+  //  }, function(err, record) {
+  //    if (err) throw err;
+  //
+  //    console.log("id:" + req.params.id);
+  //
+  //    res.redirect("/homeold");
+  //  });
+  //});
+  //
+  //
+  //// Add the record data to database, from POST on form submit
+  //router.post('/entryinput', isAuthenticated, function(req, res) {
+  //  // below outputs full response to browser in json format
+  //  //res.json(req.body);
+  //  var record = new entrySchema();
+  //
+  //  record.timestamp = req.body.timestamp;
+  //  record.user = req.body.user;
+  //  record.minutes = req.body.minutes;
+  //  record.comments = req.body.comments;
+  //
+  //  record.save(function(err) {
+  //    if (err) {
+  //      console.log(err);
+  //      res.status(500).json({
+  //        status: 'failure'
+  //      });
+  //    } else {
+  //      //res.json({status: 'success'});
+  //      res.redirect('/homeold');
+  //    }
+  //  });
+  //});
+  //
+  //router.get('/entrydelete/:id', isAuthenticated, function(req, res) {
+  //
+  //  console.log("In entry Delete");
+  //  entrySchema.find({
+  //    _id: req.params.id
+  //  }, function(err, docs) {
+  //    if (err) return console.log(err);
+  //    if (!docs || !Array.isArray(docs) || docs.length === 0)
+  //      return console.log('no docs found');
+  //
+  //    docs.forEach(function(doc) {
+  //      doc.remove();
+  //      res.redirect("/homeold");
+  //    });
+  //
+  //  });
+  //});
+
+  //return router;
+module.exports = router;
+//}
